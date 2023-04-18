840e25dc9b9f757469905471a3a431d3254adc8f
Ivan Eremin <ereminiu@mail.ru>
2023-04-18 12:07:06 +0300

    Increase custom code lenght limit

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -8,7 +8,7 @@ create index hashcode on links (hcode);
 
 create table customlinks (
     id serial not null,
-    custom varchar(10) not null,
+    custom varchar(40) not null,
     original text not null
 );
 
